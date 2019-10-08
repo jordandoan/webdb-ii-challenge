5290215c0a6a0f9ab25b2ff53be0e5211146eb70
Jordan <jordandoan@hotmail.com>
2019-10-08 10:43:37 -0700

    Implemented put and delete, and get by id

--- index.js
+++ index.js
@@ -8,12 +8,48 @@ server.use(express.json());
 server.get('/', (req,res) => {
   db('cars')
     .then(cars => res.status(200).json(cars));
-})
+});
+
+server.get('/:id', (req,res) => {
+  db('cars').where({id: req.params.id})
+    .then(car => {
+      if (car) {
+        res.status(200).json(car)
+      } else {
+        res.status(400).json({message:"Car with specified ID not found"})
+      }
+    })
+});
+
 server.post('/', (req,res) => {
   const car = req.body;
   db('cars').insert(car)
     .then(id => res.status(201).json({id:id[0], ...car}));
-})
+});
+
+server.put('/:id', (req,res) => {
+  const car = req.body;
+  db('cars').where({id: req.params.id}).update(car)
+    .then(records => {
+      if (records) {
+        res.status(201).json({id: id, ...car})
+      } else {
+        res.status(400).json({message: "bad request"})
+      }
+    })
+});
+
+server.delete('/:id', (req,res) => {
+  db('cars').where({id: req.params.id})
+    .then(records => {
+      if (records) {
+        res.status(201).json({message:"Successfully deleted!"})
+      } else {
+        res.status(400).json({message: "bad request"})
+      }
+    })
+});
+
 server.listen(port, () => {
   console.log("Server is up!");
 })
