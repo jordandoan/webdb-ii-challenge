193fda367134592d7b3634131fee78f9bd3cb98b
Jordan <jordandoan@hotmail.com>
2019-10-08 09:58:53 -0700

    Get and Post requests complete

--- index.js
+++ index.js
@@ -9,7 +9,11 @@ server.get('/', (req,res) => {
   db('cars')
     .then(cars => res.status(200).json(cars));
 })
-
+server.post('/', (req,res) => {
+  const car = req.body;
+  db('cars').insert(car)
+    .then(id => res.status(201).json({id:id[0], ...car}));
+})
 server.listen(port, () => {
   console.log("Server is up!");
 })
